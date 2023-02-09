8a2d7e8db4b37f6cc824380f9e5446f5c60951e1
ridhoafni.dev <ridhoafni.dev@gmail.com>
2023-02-09 12:50:50 +0700

    first commit

---
+++ database/migrations/2023_01_27_230623_create_food_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('ingredients')->nullable();
+            $table->string('price')->nullable();
+            $table->string('rate')->nullable();
+            $table->string('type')->nullable();
+            $table->string('picturePath')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+}
